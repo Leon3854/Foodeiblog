35475440ed492e5106b2380ff8f78e6eb54b4b76
Leon3854 <38610733+Leon3854@users.noreply.github.com>
2025-06-23 13:10:07 +0300

    Adding new Commits for this  the hook useHamburgerMenu.tsx
    Добавил комментарии к хуку

--- client/src/hooks/hamburger-menu/useHamburgerMenu.tsx
+++ client/src/hooks/hamburger-menu/useHamburgerMenu.tsx
@@ -2,20 +2,25 @@ import { useEffect } from 'react';
 
 const useHamburgerMenu = () => {
     useEffect(() => {
+        // Создание констант
         const hamburgerOpen = document.querySelector(".humberger__open");
         const hamburgerMenuWrapper = document.querySelector(".humberger__menu__wrapper");
         const hamburgerMenuOverlay = document.querySelector(".humberger__menu__overlay");
 
+        
         const openMenu = () => {
+            // тут добавляем классы
             if (hamburgerMenuWrapper) {
                 hamburgerMenuWrapper.classList.add("show__humberger__menu__wrapper");
             }
+            // если есть данная константа добваляем класс active
             if (hamburgerMenuOverlay) {
                 hamburgerMenuOverlay.classList.add('active');
             }
         };
 
         const closeMenu = () => {
+            // тут удалеяем классы
             if (hamburgerMenuWrapper) {
                 hamburgerMenuWrapper.classList.remove("show__humberger__menu__wrapper");
             }
@@ -24,10 +29,11 @@ const useHamburgerMenu = () => {
             }
         };
 
+        // через опциональную цепочку обрабтываем событие открытие меню и закрытия
         hamburgerOpen?.addEventListener('click', openMenu);
         hamburgerMenuOverlay?.addEventListener('click', closeMenu);
 
-        // Убираем обработчики событий при размонтировании компонента
+        // Убираем обработчики событий при размонтировании компонента через ту же опциональную цепочку
         return () => {
             hamburgerOpen?.removeEventListener('click', openMenu);
             hamburgerMenuOverlay?.removeEventListener('click', closeMenu);
