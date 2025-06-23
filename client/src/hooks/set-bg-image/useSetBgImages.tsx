be54999730d15c81307041ce87d709bf1f24d8c9
Leon3854 <38610733+Leon3854@users.noreply.github.com>
2025-06-23 16:35:08 +0300

    Adding new comments for the useSetBgImages.tsx

--- client/src/hooks/set-bg-image/useSetBgImages.tsx
+++ client/src/hooks/set-bg-image/useSetBgImages.tsx
@@ -1,9 +1,15 @@
 import {useEffect} from 'react'
 
+// Кастомный хук для установки фоновых изображений на элементы по заданному селектору
+// Использует хук useEffect из React для выполнения эффекта при монтировании и при изменении селектора
 const useSetBgImages = (selector) => {
 	useEffect(() => {
 			const setBgImages = () => {
+				// Получаем все элементы, соответствующие селектору
 					const bgElements = document.querySelectorAll(selector);
+					
+					// Для каждого элемента устанавливаем CSS-свойство backgroundImage
+					// Значение берется из атрибута data-setbg
 					bgElements.forEach((element) => {
 							const bg = element.getAttribute('data-setbg');
 							element.style.backgroundImage = `url(${bg})`;
@@ -11,7 +17,7 @@ const useSetBgImages = (selector) => {
 			};
 
 			setBgImages();
-	}, [selector]); // Добавляем selector в зависимости, если он изменится
+	}, [selector]); // Запускаем эффект при изменении selector
 };
 
 export default useSetBgImages
